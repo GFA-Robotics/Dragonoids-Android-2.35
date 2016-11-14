f578cb948cd8d9d396454530aeb4795aba5bb42e
Scott Barnet <barnets@gfacademy.org>
2016-11-14 17:10:07 -0500

    Added Function to test color in Dragonoids Test Color

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DragonoidsTestColor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DragonoidsTestColor.java
@@ -4,5 +4,58 @@ package org.firstinspires.ftc.teamcode;
  * Created by Dragonoids on 11/14/2016.
  */
 
-public class DragonoidsTestColor {
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
+import com.qualcomm.ftcrobotcontroller.R;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+
+@Autonomous(name = "Dragonoids Color Test", group = "Sensor")
+public class DragonoidsTestColor extends LinearOpMode {
+
+    ColorSensor colorSensor;
+
+    public void runOpMode() {
+
+        // hsvValues is an array that will hold the hue, saturation, and value information.
+        float hsvValues[] = {0F,0F,0F};
+
+        // values is a reference to the hsvValues array.
+        final float values[] = hsvValues;
+
+        // get a reference to our ColorSensor object.
+        colorSensor = hardwareMap.colorSensor.get("sensor_color");
+
+        // wait for the start button to be pressed.
+        waitForStart();
+
+        // while the op mode is active, loop and read the RGB data.
+        // Note we use opModeIsActive() as our loop condition because it is an interruptible method.
+        while (opModeIsActive()) {
+
+            // convert the RGB values to HSV values.
+            Color.RGBToHSV(colorSensor.red() * 8, colorSensor.green() * 8, colorSensor.blue() * 8, hsvValues);
+
+            // send the info back to driver station using telemetry function.
+//            telemetry.addData("Clear", colorSensor.alpha());
+            telemetry.addData("Red  ", colorSensor.red());
+//            telemetry.addData("Green", colorSensor.green());
+            telemetry.addData("Blue ", colorSensor.blue());
+//            telemetry.addData("Hue", hsvValues[0]);
+
+            telemetry.update();
+        }
+    }
+
+
+
+
+
 }
