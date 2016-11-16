796699a78f18dbdb7d41f1be303f56ad387c4fae
Scott Barnet <barnets@gfacademy.org>
2016-11-16 15:15:10 -0500

    Added Turn Function
    Added import statements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DragonoidsAutoEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DragonoidsAutoEncoder.java
@@ -1,10 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.graphics.Color;
+import android.hardware.SensorEventListener;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.HardwareDevice;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import static java.lang.Thread.sleep;
 
 /**
  * Created by Dragonoids on 11/5/2016.
@@ -16,6 +24,8 @@ public class DragonoidsAutoEncoder extends LinearOpMode {
     DcMotor motorLF;
     DcMotor motorLB;
 
+    ColorSensor colorSensor;
+
     final static int ENCODER_CPR = 1120;
     final static double WHEEL_CIRC = 4 * Math.PI;
     // go forward 2 tiles
@@ -25,6 +35,16 @@ public class DragonoidsAutoEncoder extends LinearOpMode {
     final static double INITDIST = ENCODER_CPR * ROTATE;
 
     long totTime = 0;
+
+    boolean color;
+
+    // hsvValues is an array that will hold the hue, saturation, and value information.
+    float hsvValues[] = {0F,0F,0F};
+
+    // values is a reference to the hsvValues array.
+    final float values[] = hsvValues;
+
+    public void runOpMode() throws InterruptedException {
 
         waitForStart();
 
@@ -44,6 +64,8 @@ public class DragonoidsAutoEncoder extends LinearOpMode {
 
         forward(.75, .5, 500); // move forward to give us space to rotate
 
+
+    public void resetEncoders() {
         motorRF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorRB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorLF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -75,6 +97,30 @@ public class DragonoidsAutoEncoder extends LinearOpMode {
 
         sleep(Math.abs(totTime));
 
+    }
+
+    public void turn (double distance, double power, long totTime) {
+        resetEncoders();
+        distance = ENCODER_CPR * ROTATE * distance * 1.5 / 180;
+
+        totTime = (long) (distance/power) + totTime;
+
+        motorRF.setTargetPosition((int) (distance));
+        motorRB.setTargetPosition((int) (distance));
+        motorLF.setTargetPosition((int) (-distance));
+        motorLB.setTargetPosition((int) (-distance));
+
+        motorRF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorRB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorLF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorLB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        motorRF.setPower(power);
+        motorRB.setPower(power);
+        motorLF.setPower(power);
+        motorLB.setPower(power);
+
+        sleep(Math.abs(totTime));
     }
 
 
