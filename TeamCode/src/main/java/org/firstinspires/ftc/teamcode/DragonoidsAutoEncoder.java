dc8254b3adca2356e5bfe6ecec0adf24111f560e
Scott Barnet <barnets@gfacademy.org>
2016-11-16 15:11:40 -0500

    Added forward function
    Forward is in tile integers
    Included calculations for time of movement
    Change extends opmode to linearopmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DragonoidsAutoEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DragonoidsAutoEncoder.java
@@ -9,8 +9,8 @@ import com.qualcomm.robotcore.hardware.HardwareDevice;
 /**
  * Created by Dragonoids on 11/5/2016.
  */
-@Autonomous(name="Encoder Drive", group="Linear Opmode")
-public class DragonoidsAutoEncoder extends OpMode {
+@Autonomous(name="Auto Red Position 1", group="Linear Opmode")
+public class DragonoidsAutoEncoder extends LinearOpMode {
     DcMotor motorRF;
     DcMotor motorRB;
     DcMotor motorLF;
@@ -21,21 +21,28 @@ public class DragonoidsAutoEncoder extends OpMode {
     // go forward 2 tiles
     final static int TILE = 23;
 
-    final static double TILEROTATE = TILE / WHEEL_CIRC;
-    final static double INITDIST = ENCODER_CPR * TILEROTATE;
+    final static double ROTATE = TILE / WHEEL_CIRC;
+    final static double INITDIST = ENCODER_CPR * ROTATE;
+
+    long totTime = 0;
+
+        waitForStart();
 
 
 
+        // get a reference to our ColorSensor object.
+        colorSensor = hardwareMap.colorSensor.get("sensor_color");
 
-    public void init() {
         motorRF = hardwareMap.dcMotor.get("right_drive_front");
         motorRB = hardwareMap.dcMotor.get("right_drive_back");
         motorLF = hardwareMap.dcMotor.get("left_drive_front");
         motorLB = hardwareMap.dcMotor.get("left_drive_back");
 
         //starts backwards and drives backwards
-        motorLF.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
-        motorLB.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        motorRF.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
+        motorRB.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+
+        forward(.75, .5, 500); // move forward to give us space to rotate
 
         motorRF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorRB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -43,13 +50,33 @@ public class DragonoidsAutoEncoder extends OpMode {
         motorLB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
-    public void start() {
+    public void forward (double distance, double power, long totTime) {
+        resetEncoders();
 
-        //runs 1 tile forward
-        motorRF.setTargetPosition((int) INITDIST);
-        motorRB.setTargetPosition((int) INITDIST);
-        motorLF.setTargetPosition((int) INITDIST);
-        motorLB.setTargetPosition((int) INITDIST);
+
+        distance = ENCODER_CPR * ROTATE * distance;
+
+        totTime = (long) (distance/power) + totTime;
+
+        motorRF.setTargetPosition((int) distance);
+        motorRB.setTargetPosition((int) distance);
+        motorLF.setTargetPosition((int) distance);
+        motorLB.setTargetPosition((int) distance);
+
+        motorRF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorRB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorLF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorLB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        motorRF.setPower(power);
+        motorRB.setPower(power);
+        motorLF.setPower(power);
+        motorLB.setPower(power);
+
+        sleep(Math.abs(totTime));
+
+    }
+
 
         motorRF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         motorRB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -60,17 +87,10 @@ public class DragonoidsAutoEncoder extends OpMode {
         motorRB.setPower(0.75);
         motorLF.setPower(0.75);
         motorLB.setPower(0.75);
-/*
-        //strafes to the right
-        motorRF.setTargetPosition((int) STRAFDIST);
-        motorRB.setTargetPosition((int) STRAFDIST);
-        motorLF.setTargetPosition((int) STRAFDIST);
-        motorLB.setTargetPosition((int) STRAFDIST);
-        */
+
+        sleep(Math.abs(totTime));
     }
 
-    public void loop() {
-        telemetry.addData("Distance Traveled: ", motorLF.getCurrentPosition() * (WHEEL_CIRC / ENCODER_CPR));
         telemetry.update();
     }
 }
