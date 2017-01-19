1f07ea02e873624a3c44d22a26a7cf07f867886f
Scott Barnet <barnets@gfacademy.org>
2017-01-19 17:22:16 -0500

    Finished red side button autonomous
    goes forward, shoots 2 balls, hits the cap ball, and hits both buttons

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DragonoidsAutoRed1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DragonoidsAutoRed1.java
@@ -10,45 +10,46 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @Autonomous(name="Red Autonomous Button", group="Red")
 public class DragonoidsAutoRed1 extends DragonoidsAuto {
 
+
     @Override
     public void runOpMode() throws InterruptedException {
         super.runOpMode();
         waitForStart();
+
         while (opModeIsActive()) {
+            //red side full autonomous
+            forward(-.93,.6);
             shoot();
-
-            forward(-.5, 1);
-
-            turn(-120, 1);
-            forward(2.5, 1);
-            turn(-30, .5);
-            telemetry.addData("Gyro Heading", gyro.getIntegratedZValue());
-            telemetry.update();
-            targetAngle = -180;
-            adjustRange();
+            forward(-.5,.6);
+            strafe(-.5, .6);
+            sleep(250);
+            strafe(-.25, .8);
+            turn(-180);
+            leftDiagonal(1.75, .75);
             adjustHeading();
-            alignLine(true);
-//        //false is red true is blue
-            if (!detectColor()) {
-                strafe(.5, 1);
-            } else {
-                forward(.13, .4);
-                strafe(.5, 1);
-            }
-            strafe(-.8, 1);
-            targetAngle = -180;
-            // adjustHeading();
-            forward(1.5, 1);
             adjustRange();
             alignLine(false);
             if (!detectColor()) {
+                strafe(.6, 1);
+            } else {
+                forward(.13, .4);
+                strafe(.6, 1);
+            }
+            strafe(-.5,1);
+            forward(-1.5,1);
+            adjustRange();
+            adjustHeading();
+            alignLine(true);
+            if (!detectColor()) {
                 strafe(.5, 1);
             } else {
                 forward(.13, .4);
                 strafe(.5, 1);
             }
-            strafe(-.75, 1);
-            forward(-.75, 1);
+            adjustRange();
+            turn(0);
+            leftDiagonal(-.4,.5);
+
             break;
         }
     }
