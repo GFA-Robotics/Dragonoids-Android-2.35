10a3a1dbd2f5c197b7b373738583f0d6872d5b35
Scott Barnet <barnets@gfacademy.org>
2017-01-27 15:39:36 -0500

    Scale input in new teleop test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMecanumOriginal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMecanumOriginal.java
@@ -211,6 +211,44 @@ public class TeleOpMecanumOriginal extends LinearOpMode {
         motorLB.setPower(LB);
     }
 
+    private float scaleInputOriginal(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.45, 0.52, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        // return scaled value.
+        return (float)dScale;
+    }
+    private float scaleInput (double value) {
+        // Return the value (from -1 to 1) squared to scale it quadratically
+        float magnitude = (float) Math.pow(value, 2);
+        if (value < 0) {
+            return -1 * magnitude;
+        }
+        else {
+            return magnitude;
+        }
+    }
     public boolean detectColor () {
 
         //false is red
