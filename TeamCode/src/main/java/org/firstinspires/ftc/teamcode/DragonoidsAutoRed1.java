161f0b35d0c4fb746d9170f7ec722c71f575deec
Scott Barnet <barnets@gfacademy.org>
2017-03-03 18:46:19 -0500

    Finalized values for new Red path

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DragonoidsAutoRed1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DragonoidsAutoRed1.java
@@ -16,8 +16,8 @@ public class DragonoidsAutoRed1 extends DragonoidsAuto {
         super.runOpMode();
 
         while (opModeIsActive()) {
-            forward(1.45,.6);
-            strafe(1, .8);
+            forward(1.5,.6);
+            strafe(1.25, .8);
             forward(2.2,1);
             adjustHeading();
             adjustRange();
@@ -32,7 +32,7 @@ public class DragonoidsAutoRed1 extends DragonoidsAuto {
                 strafe(.75, 1);
             }
             adjustRange();
-            forward(-1.7,.5);
+            forward(-1.7,.65);
             adjustRange();
             adjustHeading();
             alignLine(true);
@@ -45,11 +45,11 @@ public class DragonoidsAutoRed1 extends DragonoidsAuto {
                 sleep(250);
                 strafe(.75, 1);
             }
-            adjustRange();
-            turn(90);
+            strafe(-.9,.8);
+            turn(100);
             shoot();
-            strafe(-.5,1);
-            forward(-1.55,.8);
+            strafe(-.65,1);
+            forward(-1.45,.8);
 
             break;
         }
